261f110c10d6e6eff205fc366e1aad1ec6dfa838
Alex Nagy <therebeloper@gmail.com>
2020-02-13 13:12:07 +0200

    ViewController

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SparkUI",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DeclarativeHub/Layoutless.git", from: "0.4.1"),
+        .package(url: "https://github.com/rebeloper/Layoutless.git", from: "0.4.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ Sources/SparkUI/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  
+//
+//  Created by Alex Nagy on 13/02/2020.
+//
+
+import UIKit
+import Layoutless
+
+/// Spark ViewController with predifend, commonly used elements
+class ViewController: UIViewController {
+    
+    /// ViewController's container view to add any view onto it
+    public let container = UIView()
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        layoutViews()
+        bind()
+        observe()
+    }
+    
+    /// layout views onto the 'container'
+    open func layoutViews(relativeToSafeArea: Bool = true) {
+        stack(.vertical)(
+            container
+            ).fillingParent(relativeToSafeArea: relativeToSafeArea).layout(in: view)
+    }
+    
+    /// bind your views here
+    open func bind() {}
+    
+    /// observe your views here
+    open func observe() {}
+}
