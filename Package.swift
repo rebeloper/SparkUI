7c8e88d4bd09ee7d88869f8aea8bd11ded0eedc0
Alex Nagy <therebeloper@gmail.com>
2020-02-13 21:07:03 +0200

    added ReactiveKit

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         .package(url: "https://github.com/rebeloper/Layoutless.git", .upToNextMajor(from: "0.0.0")),
         .package(url: "https://github.com/rebeloper/SparkExtensions.git", .upToNextMajor(from: "0.0.0")),
         .package(url: "https://github.com/JonasGessner/JGProgressHUD", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/DeclarativeHub/ReactiveKit.git", .upToNextMajor(from: "3.10.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,8 @@ let package = Package(
             dependencies: [
                 "LayoutlessRebeloper",
                 "SparkExtensions",
-                "JGProgressHUD"
+                "JGProgressHUD",
+                "ReactiveKit"
         ]),
         .testTarget(
             name: "SparkUITests",
