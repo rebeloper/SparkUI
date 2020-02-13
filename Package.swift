58c58b066839b272431cc493b869fcb83b5d1426
Alex Nagy <therebeloper@gmail.com>
2020-02-13 13:35:01 +0200

    name fix

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SparkUI",
-            dependencies: ["Layoutless"]),
+            dependencies: ["LayoutlessRebeloper"]),
         .testTarget(
             name: "SparkUITests",
             dependencies: ["SparkUI"]),
