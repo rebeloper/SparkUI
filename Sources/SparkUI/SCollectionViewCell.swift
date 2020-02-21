1ae83633977fbdeaa626bb629fbf3be060a62b2f
Alex Nagy <therebeloper@gmail.com>
2020-02-21 16:01:19 +0200

    Collection Views

---
+++ Sources/SparkUI/SCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SCollectionViewCell.swift
+//  
+//
+//  Created by Alex Nagy on 21/02/2020.
+//
+
+import UIKit
+import SparkExtensions
+
+public class SCollectionViewCell: UICollectionViewCell {
+    
+    public let container = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        container.backgroundColor = .systemBackground
+        layoutViews()
+        bind()
+        observe()
+        continueInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open func layoutViews() {
+        addSubview(container)
+        container.edgeTo(self)
+    }
+    
+    open func bind() {}
+    open func observe() {}
+    open func continueInit() {}
+}
---
+++ Sources/SparkUI/SelfConfiguringCell.swift
@@ -0,0 +1,12 @@
+//
+//  SelfConfiguringCell.swift
+//  
+//
+//  Created by Alex Nagy on 21/02/2020.
+//
+
+import Foundation
+
+protocol SelfConfiguringCell {
+    static var reuseIdentifier: String { get }
+}
