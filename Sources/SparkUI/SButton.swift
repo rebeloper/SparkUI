42767299717db7b179595c00f100057b973b623b
Alex Nagy <therebeloper@gmail.com>
2020-02-23 19:06:23 +0200

    activity indicator text

--- Sources/SparkUI/SButton.swift
+++ Sources/SparkUI/SButton.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
-import SparkMisc
+import Layoutless
+
 
 public class SButton: UIView {
     
     public var object: UIView?
     var activityIndicatorView = UIActivityIndicatorView()
+    var activityIndicatorViewMessageLabel = UILabel()
     
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
@@ -33,25 +35,43 @@ public class SButton: UIView {
         super.init(frame: .zero)
         
         addSubview(uiView)
-        addSubview(activityIndicatorView)
-        
         uiView.edgeTo(self)
-        activityIndicatorView.centerIn(self)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func startActivityIndicator() {
+    public func startActivityIndicator(text: String = "") {
         isUserInteractionEnabled = false
-        self.object?.addFadeTo(0.2)
+        
+        activityIndicatorViewMessageLabel.isHidden(false)
+        
+        if text != "" {
+            self.object?.addFadeTo(0.0, duration: 0.0)
+            stack(.horizontal, spacing: 10)(
+            activityIndicatorView,
+            activityIndicatorViewMessageLabel.text(text)
+            ).centeringInParent().layout(in: self)
+        } else {
+            self.object?.addFadeTo(0.2, duration: 0.0)
+            stack(.horizontal)(
+                activityIndicatorView
+                ).centeringInParent().layout(in: self)
+        }
+        
         activityIndicatorView.startAnimating()
+    }
+    
+    public func updateActivityIndicator(text: String) {
+        activityIndicatorViewMessageLabel.text(text)
     }
     
     public func stopActivityIndicator() {
         isUserInteractionEnabled = true
         self.object?.addFadeTo(1.0, duration: 0.2, completion: nil)
+        activityIndicatorViewMessageLabel.isHidden(true)
+        
         activityIndicatorView.stopAnimating()
     }
 }
