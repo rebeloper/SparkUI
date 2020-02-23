3524c7dd7fae92f6cf4ed76917163e092c644073
Alex Nagy <therebeloper@gmail.com>
2020-02-23 18:16:30 +0200

    bump

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/DeclarativeHub/Layoutless.git", .upToNextMajor(from: "0.4.2")),
-        .package(url: "https://github.com/rebeloper/SparkExtensions.git", .upToNextMajor(from: "0.0.21")),
+        .package(url: "https://github.com/rebeloper/SparkExtensions.git", .upToNextMajor(from: "0.0.22")),
         .package(url: "https://github.com/rebeloper/SparkMisc.git", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/JonasGessner/JGProgressHUD", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/DeclarativeHub/ReactiveKit.git", .upToNextMajor(from: "3.10.0")),
