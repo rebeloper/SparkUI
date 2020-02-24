cab6299703f96a8b1f5544c33c68b5f3525127e1
Alex Nagy <therebeloper@gmail.com>
2020-02-24 11:19:51 +0200

    init

--- Sources/SparkUI/SViewController.swift
+++ Sources/SparkUI/SViewController.swift
@@ -10,20 +10,31 @@ import SparkExtensions
 
 open class SViewController: UIViewController {
     
+    public var safeArea: SSafeArea
+    
     public let hud = SHud.create()
     
     public let container = UIView()
     
+    public init(_ safeArea: SSafeArea = .all) {
+        self.safeArea = safeArea
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
         container.backgroundColor = .systemBackground
-        layoutViews(safeArea: .all)
+        layoutViews()
         bind()
         observe()
         continueViewDidLoad()
     }
     
-    open func layoutViews(safeArea: SSafeArea) {
+    open func layoutViews() {
         view.addSubview(container)
         container.edgeTo(view, safeArea: safeArea)
     }
