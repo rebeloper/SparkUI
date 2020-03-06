61aa42b26b52fce5ba3d77a5fcb487f452d581df
Alex Nagy <therebeloper@gmail.com>
2020-03-06 10:55:43 +0200

    STextFieldView

--- Sources/SparkUI/STextFieldView.swift
+++ Sources/SparkUI/STextFieldView.swift
@@ -6,27 +6,80 @@
 //
 
 import UIKit
+import ReactiveKit
+import Bond
 import Layoutless
 
 public class STextFieldView: UIView {
     
     public var object: STextField?
+    private let eyeButton = UIButton().image(UIImage(systemName: "eye.slash.fill"))
+    private let showingSecureText = Property(false)
     
-    public init(sTextField: STextField, underlined: Bool = false) {
+    public init(sTextField: STextField, isSecure: Bool = false, underlined: Bool = true, underlineSpacing: CGFloat = 5) {
         self.object = sTextField
         super.init(frame: .zero)
         
-        if underlined {
-            let underline = SDivider()
-            stack(.vertical)(
-                sTextField,
-                Spacer().setHeight(3),
-                underline
+        if isSecure {
+            
+            if underlined {
+                let underline = SDivider()
+                stack(.vertical)(
+                    stack(.horizontal, spacing: 5)(
+                        sTextField.isSecureTextEntry(true),
+                        eyeButton
+                    ),
+                    Spacer().setHeight(underlineSpacing),
+                    underline
+                    ).fillingParent().layout(in: self)
+            } else {
+                stack(.vertical)(
+                    stack(.horizontal, spacing: 5)(
+                        sTextField.isSecureTextEntry(true),
+                        eyeButton
+                    )
                 ).fillingParent().layout(in: self)
+            }
+            
+            observe()
+            
+        } else {
+            
+            if underlined {
+                let underline = SDivider()
+                stack(.vertical)(
+                    sTextField,
+                    Spacer().setHeight(underlineSpacing),
+                    underline
+                    ).fillingParent().layout(in: self)
+            } else {
+                stack(.vertical)(
+                    sTextField
+                ).fillingParent().layout(in: self)
+            }
         }
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func observe() {
+        eyeButton.reactive.tap.observeNext { [weak self] in
+            guard let self = self else { return }
+            self.showingSecureText.value.toggle()
+        }.dispose(in: bag)
+        
+        showingSecureText.observeNext { (showing) in
+            if showing {
+                self.object?.isSecureTextEntry(false)
+                self.eyeButton.image(UIImage(systemName: "eye.fill"))
+            } else {
+                self.object?.isSecureTextEntry(true)
+                self.eyeButton.image(UIImage(systemName: "eye.slash.fill"))
+            }
+        }.dispose(in: bag)
     }
 }
