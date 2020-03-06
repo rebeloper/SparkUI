77c75066f0c0e9059ba66e71802d1decf1f2fee9
Alex Nagy <therebeloper@gmail.com>
2020-03-06 14:24:34 +0200

    SApplication

---
+++ Sources/SparkUI/SApplication.swift
@@ -0,0 +1,21 @@
+//
+//  SApplication.swift
+//  
+//
+//  Created by Alex Nagy on 06/03/2020.
+//
+
+import Foundation
+
+extension SApplication {
+    
+    static func openMailApp() {
+        if let url = URL(string: "message://") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            } else {
+                SAlertController.showError(message: "Could not open Mail app")
+            }
+        }
+    }
+}
