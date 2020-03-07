4c5ac52702c97dff4a18dda76a409ab22f3656c9
Alex Nagy <therebeloper@gmail.com>
2020-03-07 11:18:14 +0200

    init

--- Sources/SparkUI/Style/SStyle.swift
+++ Sources/SparkUI/Style/SStyle.swift
@@ -52,3 +52,50 @@ extension STextField {
         return self
     }
 }
+
+/// Represents view style with a closure that configures the view.
+public struct SButtonStyle<View: SButton> {
+
+    public let style: (View) -> Void
+
+    public init(style: @escaping (View) -> Void) {
+        self.style = style
+    }
+
+    /// Applies self to the view.
+    public func apply(to view: View) {
+        style(view)
+    }
+
+    /// Style that does nothing (keeps the default/native style).
+    public static var native: SButtonStyle<View> {
+        return SButtonStyle { _ in }
+    }
+}
+
+extension SButton {
+    
+    public convenience init<V>(uiView: UIView, style: STextFieldStyle<V>) {
+        self.init(uiView: uiView)
+        apply(style)
+    }
+
+    /// Applies the given style to self.
+    public func apply<V>(_ style: SButtonStyle<V>) {
+        guard let view = self as? V else {
+            print("💥 Could not apply style for \(V.self) to \(type(of: self))")
+            return
+        }
+        style.apply(to: view)
+    }
+    
+    public func styled<V>(with style: SButtonStyle<V>) -> Self {
+        guard let view = self as? V else {
+            print("💥 Could not apply style for \(V.self) to \(type(of: self))")
+            return self
+        }
+        style.apply(to: view)
+        return self
+    }
+}
+
