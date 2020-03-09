a326b5fc2ac4400dffaa4aad3324fd1412cb62c0
Alex Nagy <therebeloper@gmail.com>
2020-03-09 16:40:52 +0200

    SMailComposeViewController

--- Sources/SparkUI/ViewControllers/SMailComposeViewController.swift
+++ Sources/SparkUI/ViewControllers/SMailComposeViewController.swift
@@ -9,25 +9,11 @@ import MessageUI
 
 public class SMailComposeViewController: MFMailComposeViewController {
     
-    public var recipients: [String]?
-    public var subject: String
-    public var messageBody: String
-    public var messageBodyIsHTML: Bool
-    
-    override open func viewDidLoad() {
-        super.viewDidLoad()
+    public init(recipients: [String]?, subject: String = "", messageBody: String = "", messageBodyIsHTML: Bool = false) {
+        super.init(nibName: nil, bundle: nil)
         setToRecipients(recipients)
         setSubject(subject)
         setMessageBody(messageBody, isHTML: messageBodyIsHTML)
-    }
-    
-    public init(recipients: [String]?, subject: String = "", messageBody: String = "", messageBodyIsHTML: Bool = false) {
-        self.recipients = recipients
-        self.subject = subject
-        self.messageBody = messageBody
-        self.messageBodyIsHTML = messageBodyIsHTML
-        
-        super.init(nibName: nil, bundle: nil)
     }
     
     required public init?(coder aDecoder: NSCoder) {
