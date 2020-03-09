593cad583173bb3cbb25a36a8c6543e682c076f2
Alex Nagy <therebeloper@gmail.com>
2020-03-09 19:24:05 +0200

    open

--- Sources/SparkUI/ViewControllers/SSignInWithAppleViewController.swift
+++ Sources/SparkUI/ViewControllers/SSignInWithAppleViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AuthenticationServices
 import CryptoKit
 
-public class SSignInWithAppleViewController: SViewController {
+open class SSignInWithAppleViewController: SViewController {
     
     // MARK: - Dependencies
     
