07c8bde08ae4073c8350831e289eb86f0b9aed5e
Alex Nagy <therebeloper@gmail.com>
2020-03-09 18:54:47 +0200

    CryptoKit

--- Sources/SparkUI/ViewControllers/SSignInWithAppleViewController.swift
+++ Sources/SparkUI/ViewControllers/SSignInWithAppleViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AuthenticationServices
+import CryptoKit
 
 public class SSignInWithAppleViewController: SViewController {
     
@@ -76,6 +77,52 @@ public class SSignInWithAppleViewController: SViewController {
         super.observe()
     }
     
+}
+
+extension SSignInWithAppleViewController {
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    static func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if length == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    @available(iOS 13, *)
+    static func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
 }
 
 extension SSignInWithAppleViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
