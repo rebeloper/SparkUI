39f105122d3eb0103e8def74b49379193db42da0
Alex Nagy <therebeloper@gmail.com>
2020-03-11 12:28:42 +0200

    Typealiases

---
+++ Sources/SparkUI/Utils/Typealiases.swift
@@ -0,0 +1,14 @@
+//
+//  Typealiases.swift
+//  
+//
+//  Created by Alex Nagy on 11/03/2020.
+//
+
+import UIKit
+
+public typealias CACornerMaskTopLeft = CACornerMask.layerMaxXMinYCorner
+public typealias CACornerMaskTopRight = CACornerMask.layerMinXMinYCorner
+public typealias CACornerMaskBottomLeft = CACornerMask.layerMinXMaxYCorner
+public typealias CACornerMaskBottomRight = CACornerMask.layerMaxXMaxYCorner
+
