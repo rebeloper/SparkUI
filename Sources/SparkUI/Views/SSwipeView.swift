a5edc56872f3924b5e4d4b2aa1c1f873ecf72e9b
Alex Nagy <therebeloper@gmail.com>
2020-03-13 15:32:53 +0200

    SSwipeView

--- Sources/SparkUI/Views/SSwipeView.swift
+++ Sources/SparkUI/Views/SSwipeView.swift
@@ -12,7 +12,7 @@ open class SSwipeView: UIView {
     
     public var object = UIView()
     
-    public init(size: CGSize = CGSize(width: 140, height: 4), color: UIColor = UIColor.systemGray.withAlphaComponent(0.5), backgroundColor: UIColor = .systemBackground, edgeInsets: UIEdgeInsets) {
+    public init(size: CGSize = CGSize(width: 100, height: 4), color: UIColor = UIColor.systemGray.withAlphaComponent(0.5), backgroundColor: UIColor = .systemBackground, edgeInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)) {
         super.init(frame: .zero)
         self.backgroundColor = backgroundColor
         
