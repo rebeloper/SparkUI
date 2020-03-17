28ae966d76d76e77f5b24b7aa93cbb8409d61965
Alex Nagy <therebeloper@gmail.com>
2020-03-17 11:32:09 +0200

    scrollToLastItemm

--- Sources/SparkUI/Extensions/UIKit/UICollectionView+.swift
+++ Sources/SparkUI/Extensions/UIKit/UICollectionView+.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 extension UICollectionView {
-    public func scrollToLastItem(at scrollPosition: UICollectionView.ScrollPosition, adjustment: CGFloat = 0.0, withAdjustmentDuration duration: TimeInterval = 0.5) {
+    
+    public func scrollToLastItemm(at scrollPosition: UICollectionView.ScrollPosition, adjustment: CGFloat = 0.0, withAdjustmentDuration duration: TimeInterval = 0.5) {
         let lastSection = self.numberOfSections - 1
         let lastRowInLastSection = self.numberOfItems(inSection: lastSection)
-        if lastSection > 0, lastRowInLastSection > 1 {
+        if lastSection > 0, lastRowInLastSection > 0 {
             let indexPath = IndexPath(row: lastRowInLastSection - 1, section: lastSection)
-            self.self.scrollToItem(at: indexPath, at: scrollPosition, animated: true)
-            UIView.animate(withDuration: duration) {
-                switch scrollPosition {
-                case .top, .bottom, .centeredVertically:
-                    self.contentOffset.y += adjustment
-                case .left, .right, .centeredHorizontally:
-                    self.contentOffset.x += adjustment
-                default:
-                    print("Inavlid scrollPosition: \(scrollPosition)")
+            let visibleIndexPaths = self.indexPathsForVisibleItems
+            if !visibleIndexPaths.contains(indexPath) {
+                self.self.scrollToItem(at: indexPath, at: scrollPosition, animated: true)
+                UIView.animate(withDuration: duration) {
+                    switch scrollPosition {
+                    case .top, .bottom, .centeredVertically:
+                        self.contentOffset.y += adjustment
+                    case .left, .right, .centeredHorizontally:
+                        self.contentOffset.x += adjustment
+                    default:
+                        print("Inavlid scrollPosition: \(scrollPosition)")
+                    }
                 }
             }
         }
