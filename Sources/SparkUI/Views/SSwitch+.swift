bd6153ab9c94921b28d169e8437335da8d25977a
Alex Nagy <therebeloper@gmail.com>
2020-03-19 20:51:07 +0200

    offTintColor

--- Sources/SparkUI/Views/SSwitch+.swift
+++ Sources/SparkUI/Views/SSwitch+.swift
@@ -10,45 +10,51 @@ import UIKit
 extension SSwitch {
     
     public func isOn() -> Bool? {
-        self.object.isOn
+        object.isOn
     }
     
     @discardableResult
     public func onTintColor(_ onTintColor: UIColor?) -> SSwitch {
-        self.object.onTintColor(onTintColor)
+        object.onTintColor(onTintColor)
+        return self
+    }
+    
+    @discardableResult
+    public func offTintColor(_ offTintColor: UIColor?) -> UISwitch {
+        object.offTintColor(offTintColor)
         return self
     }
     
     @discardableResult
     public func onImage(_ onImage: UIImage?) -> SSwitch {
-        self.object.onImage(onImage)
+        object.onImage(onImage)
         return self
     }
     
     @discardableResult
     public func offImage(_ offImage: UIImage?) -> SSwitch {
-        self.object.offImage(offImage)
+        object.offImage(offImage)
         return self
     }
     
     @discardableResult
     public func isOn(_ isOn: Bool) -> SSwitch {
-        self.object.isOn(isOn)
+        object.isOn(isOn)
         return self
     }
     
     @discardableResult
     public func setSwitchBorder(width: CGFloat, color: UIColor) -> SSwitch {
-        self.object.layer.borderWidth = width
-        self.object.layer.cornerRadius = self.object.frame.height / 2
-        self.object.layer.borderColor = color.cgColor
-        self.object.layer.masksToBounds = true
+        object.layer.borderWidth = width
+        object.layer.cornerRadius = self.object.frame.height / 2
+        object.layer.borderColor = color.cgColor
+        object.layer.masksToBounds = true
         return self
     }
     
     @discardableResult
     public func setOn(_ setOn: Bool, animated: Bool = true) -> SSwitch {
-        self.object.setOn(setOn, animated: animated)
+        object.setOn(setOn, animated: animated)
         return self
     }
     
