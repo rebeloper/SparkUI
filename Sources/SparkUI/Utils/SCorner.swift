02e2cdc92bc8a128e39e800a73a2d5395138b57c
Alex Nagy <therebeloper@gmail.com>
2020-03-26 13:36:40 +0200

    CACornerMask

--- Sources/SparkUI/Utils/SCorner.swift
+++ Sources/SparkUI/Utils/SCorner.swift
@@ -7,12 +7,50 @@
 
 import UIKit
 
-public var CACornerMask_topLeadingCorner: CACornerMask { get { return .layerMaxXMinYCorner}}
-public var CACornerMask_topTrailingCorner: CACornerMask { get { return .layerMinXMinYCorner}}
+public var CACornerMask_topLeadingCorner: CACornerMask { get { return .layerMinXMinYCorner}}
+public var CACornerMask_topTrailingCorner: CACornerMask { get { return .layerMaxXMinYCorner}}
 public var CACornerMask_bottomLeadingCorner: CACornerMask { get { return .layerMinXMaxYCorner}}
 public var CACornerMask_bottomTrailingCorner: CACornerMask { get { return .layerMaxXMaxYCorner}}
-public var CACornerMask_topCorners: CACornerMask { get { return [.layerMaxXMinYCorner, .layerMinXMinYCorner]}}
-public var CACornerMask_bottomCorners: CACornerMask { get { return [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]}}
-public var CACornerMask_leadingCorners: CACornerMask { get { return [.layerMinXMaxYCorner, .layerMaxXMinYCorner]}}
-public var CACornerMask_trailingCorners: CACornerMask { get { return [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]}}
-public var CACornerMask_allCorners: CACornerMask { get { return [.layerMinXMaxYCorner, .layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMinYCorner]}}
+
+public var CACornerMask_topCorners: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_topTrailingCorner]}}
+public var CACornerMask_bottomCorners: CACornerMask { get {
+    return [CACornerMask_bottomLeadingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+public var CACornerMask_leadingCorners: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_bottomLeadingCorner]}}
+public var CACornerMask_trailingCorners: CACornerMask { get {
+    return [CACornerMask_topTrailingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+
+public var CACornerMask_topLeading_BottomTrailingCorners: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+public var CACornerMask_topTrailing_BottomLeadingCorners: CACornerMask { get {
+    return [CACornerMask_topTrailingCorner,
+            CACornerMask_bottomLeadingCorner]}}
+
+public var CACornerMask_allCorners: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_topTrailingCorner,
+            CACornerMask_bottomLeadingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+
+public var CACornerMask_allCornersExeptTopLeading: CACornerMask { get {
+    return [CACornerMask_topTrailingCorner,
+            CACornerMask_bottomLeadingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+public var CACornerMask_allCornersExeptTopTrailing: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_bottomLeadingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+public var CACornerMask_allCornersExeptBottomLeading: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_topTrailingCorner,
+            CACornerMask_bottomTrailingCorner]}}
+public var CACornerMask_allCornersExeptBottomTrailing: CACornerMask { get {
+    return [CACornerMask_topLeadingCorner,
+            CACornerMask_topTrailingCorner,
+            CACornerMask_bottomLeadingCorner]}}
