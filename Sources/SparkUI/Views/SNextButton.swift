4b0717ffaa750ec27b9ce61da58234c7434c5ace
Alex Nagy <therebeloper@gmail.com>
2020-04-05 13:52:17 +0300

    SNextButton

---
+++ Sources/SparkUI/Views/SNextButton.swift
@@ -0,0 +1,77 @@
+//
+//  SNextButton.swift
+//  
+//
+//  Created by Alex Nagy on 05/04/2020.
+//
+
+import UIKit
+import Layoutless
+
+public class SNextButton: UIView {
+    
+    public var object: UIView!
+    public var accessoryView: UIView!
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        addFadeTo(0.2, duration: 0.0)
+    }
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        addFadeTo(1.0, duration: 0.2)
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        addFadeTo(1.0, duration: 0.2)
+    }
+    
+    public init(
+        uiView: UIView = UILabel()
+        .text("Next")
+        .text(color: .systemBlue),
+        accessoryView: UIView = UIImageView()
+        .setSystemImage("chevron.right")
+        .color(.systemBlue)
+        .size(CGSize(width: 22, height: 28))
+        .background(color: .systemBlue)) {
+        
+        object = uiView
+        self.accessoryView = accessoryView
+        super.init(frame: .zero)
+        
+        stack(.horizontal)(
+            uiView,
+            accessoryView
+            ).fillingParent().layout(in: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func enable() {
+        isUserInteractionEnabled = true
+        addFadeTo(1.0, duration: 0.2)
+    }
+    
+    public func disable() {
+        isUserInteractionEnabled = false
+        addFadeTo(0.5, duration: 0.2)
+    }
+    
+    @discardableResult
+    public func enabled() -> SButton {
+        self.enable()
+        return self
+    }
+    
+    @discardableResult
+    public func disabled() -> SButton {
+        self.disable()
+        return self
+    }
+}
+
