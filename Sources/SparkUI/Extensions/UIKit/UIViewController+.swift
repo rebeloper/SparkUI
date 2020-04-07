446c68b2a1480195e6fab157e1b6ea48d2cd74a8
Alex Nagy <therebeloper@gmail.com>
2020-04-07 18:42:05 +0300

    UIViewController+

---
+++ Sources/SparkUI/Extensions/UIKit/UIViewController+.swift
@@ -0,0 +1,41 @@
+//
+//  UIViewController+.swift
+//  
+//
+//  Created by Alex Nagy on 07/04/2020.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setTabBarVisible(_ visible: Bool, animated: Bool = true, duration: TimeInterval = 0.3) {
+
+        //* This cannot be called before viewDidLayoutSubviews(), because the frame is not set before this time
+
+        // bail if the current state matches the desired state
+        if (tabBarIsVisible() == visible) { return }
+
+        // get a frame calculation ready
+        let frame = self.tabBarController?.tabBar.frame
+        let height = frame?.size.height
+        let offsetY = (visible ? -height! : height)
+
+        // zero duration means no animation
+        let duration: TimeInterval = (animated ? duration : 0.0)
+
+        //  animate the tabBar
+        if frame != nil {
+            UIView.animate(withDuration: duration) {
+                self.tabBarController?.tabBar.frame = frame!.offsetBy(dx: 0, dy: offsetY!)
+                return
+            }
+        }
+        
+    }
+
+    func tabBarIsVisible() -> Bool {
+        return (self.tabBarController?.tabBar.frame.origin.y)! < self.view.frame.maxY
+    }
+
+}
+
