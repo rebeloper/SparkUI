c3d96d13aab49029a3524547be75c64114b10b44
Alex Nagy <therebeloper@gmail.com>
2020-04-14 13:05:07 +0300

    underline spacing

--- Sources/SparkUI/Views/STextField.swift
+++ Sources/SparkUI/Views/STextField.swift
@@ -29,7 +29,7 @@ public class STextField: UIView {
                 sTextFieldBase: STextFieldBase = STextFieldBase(),
                 isSecure: Bool = false,
                 underlined: Bool = false,
-                underlineSpacing: CGFloat = 5,
+                underlineSpacing: CGFloat = 6,
                 actionButton: SNextButton? = nil) {
         self.placeholder = placeholder
         self.placeholderAnimation = placeholderAnimation
