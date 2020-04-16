654630354598cf68913264b8b2674995fd4cd8dc
Alex Nagy <therebeloper@gmail.com>
2020-04-16 11:52:46 +0300

    maximumNumberOfLines

--- Sources/SparkUI/Views/STextView.swift
+++ Sources/SparkUI/Views/STextView.swift
@@ -31,27 +31,16 @@ extension STextView {
         return self
     }
     
-    public static func applyLimits(maximumNumberOfLines: Int = Int.max, maximumNumberOfCharacters: Int = Int.max, textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
-        let numberOfCharacters = newText.count
-        if numberOfCharacters <= maximumNumberOfCharacters {
-            let existingLines = textView.text.components(separatedBy: CharacterSet.newlines)
-            let newLines = text.components(separatedBy: CharacterSet.newlines)
-            let linesAfterChange = existingLines.count + newLines.count - 1
-//            if(text == "\n") {
-//                return linesAfterChange <= maximumNumberOfLines
-//            } else {
-//                return true
-//            }
-//            return linesAfterChange <= maximumNumberOfLines
-            if linesAfterChange <= maximumNumberOfLines {
-                return true
-            } else {
-                return false
-            }
-        } else {
-            return false
-        }
+    @discardableResult
+    open func maximumNumberOfLines(maximumNumberOfLines: Int) -> STextView {
+        textContainer.maximumNumberOfLines = maximumNumberOfLines
+        return self
+    }
+    
+    @discardableResult
+    open func lineBreakMode(lineBreakMode: NSLineBreakMode) -> STextView {
+        textContainer.lineBreakMode = lineBreakMode
+        return self
     }
     
     @discardableResult
