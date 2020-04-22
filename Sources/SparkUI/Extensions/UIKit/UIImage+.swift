af7da8a36df650650f0ef9877fa612782b376796
Alex Nagy <therebeloper@gmail.com>
2020-04-23 01:32:13 +0300

    UIImage+

---
+++ Sources/SparkUI/Extensions/UIKit/UIImage+.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+.swift
+//  
+//
+//  Created by Alex Nagy on 23/04/2020.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    public func resized(withPercentage percentage: CGFloat, isOpaque: Bool = true) -> UIImage? {
+        let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+    
+    public func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
+        let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+}
+
