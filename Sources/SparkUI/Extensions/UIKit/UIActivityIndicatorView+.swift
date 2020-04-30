e919b4bffb4c66d50790e0a9adbb0c64d139d84c
Alex Nagy <therebeloper@gmail.com>
2020-04-30 11:23:23 +0300

    UIActivityIndicatorView+

---
+++ Sources/SparkUI/Extensions/UIKit/UIActivityIndicatorView+.swift
@@ -0,0 +1,30 @@
+//
+//  UIActivityIndicatorView+.swift
+//  
+//
+//  Created by Alex Nagy on 30/04/2020.
+//
+
+import UIKit
+
+extension UIActivityIndicatorView {
+    
+    @discardableResult
+    public func color(_ color: UIColor) -> UIActivityIndicatorView {
+        self.color = color
+        return self
+    }
+    
+    @discardableResult
+    public func style(_ style: UIActivityIndicatorView.Style) -> UIActivityIndicatorView {
+        self.style = style
+        return self
+    }
+    
+    @discardableResult
+    public func hidesWhenStopped(_ hidesWhenStopped: Bool = true) -> UIActivityIndicatorView {
+        self.hidesWhenStopped = hidesWhenStopped
+        return self
+    }
+    
+}
