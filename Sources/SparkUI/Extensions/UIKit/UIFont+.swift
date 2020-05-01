d48cce566e2cc4bddfd9530914f1811d130a35a2
Alex Nagy <therebeloper@gmail.com>
2020-05-01 13:53:44 +0300

    public

--- Sources/SparkUI/Extensions/UIKit/UIFont+.swift
+++ Sources/SparkUI/Extensions/UIKit/UIFont+.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIFont {
-    func insert(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+    public func insert(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
         if fontDescriptor.symbolicTraits.contains(traits) {
             return self
         } else {
@@ -19,7 +19,7 @@ extension UIFont {
         }
     }
     
-    func remove(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+    public func remove(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
         if !fontDescriptor.symbolicTraits.contains(traits) {
             return self
         } else {
@@ -32,17 +32,17 @@ extension UIFont {
 }
 
 extension UIFont {
-    var ultraLight: UIFont { return withWeight(.ultraLight) }
-    var thin: UIFont { return withWeight(.thin) }
-    var light: UIFont { return withWeight(.light) }
-    var regular: UIFont { return withWeight(.regular) }
-    var medium: UIFont { return withWeight(.medium) }
-    var semibold: UIFont { return withWeight(.semibold) }
-    var bold: UIFont { return withWeight(.bold) }
-    var heavy: UIFont { return withWeight(.heavy) }
-    var black: UIFont { return withWeight(.black) }
+    public var ultraLight: UIFont { return withWeight(.ultraLight) }
+    public var thin: UIFont { return withWeight(.thin) }
+    public var light: UIFont { return withWeight(.light) }
+    public var regular: UIFont { return withWeight(.regular) }
+    public var medium: UIFont { return withWeight(.medium) }
+    public var semibold: UIFont { return withWeight(.semibold) }
+    public var bold: UIFont { return withWeight(.bold) }
+    public var heavy: UIFont { return withWeight(.heavy) }
+    public var black: UIFont { return withWeight(.black) }
 
-    func withWeight(_ weight: UIFont.Weight) -> UIFont {
+    public func withWeight(_ weight: UIFont.Weight) -> UIFont {
         var attributes = fontDescriptor.fontAttributes
         var traits = (attributes[.traits] as? [UIFontDescriptor.TraitKey: Any]) ?? [:]
 
