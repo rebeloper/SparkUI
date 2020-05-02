4c54efb100c037f2934f4f9a026d0386bcc39b7b
Alex Nagy <therebeloper@gmail.com>
2020-05-03 00:27:08 +0300

    public

--- Sources/SparkUI/Views/SVisualEffectView.swift
+++ Sources/SparkUI/Views/SVisualEffectView.swift
@@ -28,7 +28,7 @@ open class SVisualEffectView: UIVisualEffectView {
         animator.fractionComplete = 1.0
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
