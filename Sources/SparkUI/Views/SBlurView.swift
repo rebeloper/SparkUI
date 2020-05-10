be1d431a11e27c2d2ef5673b534e286c77e0aa5e
Alex Nagy <therebeloper@gmail.com>
2020-05-10 20:39:57 +0300

    SBlurView

--- Sources/SparkUI/Views/SBlurView.swift
+++ Sources/SparkUI/Views/SBlurView.swift
@@ -34,30 +34,3 @@ open class SBlurView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-open class SVisualEffectView: UIVisualEffectView {
-    
-    private var animator: UIViewPropertyAnimator!
-    private let blurEffect = (NSClassFromString("_UICustomBlurEffect") as! UIBlurEffect.Type).init()
-    
-    public init(style: UIBlurEffect.Style, intensity: CGFloat) {
-        super.init(effect: nil)
-        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = UIBlurEffect(style: style) }
-        animator.fractionComplete = intensity
-    }
-    
-    public init(color: UIColor? = nil, alpha: CGFloat = 0.5, blurRadius: CGFloat = 3, scale: CGFloat = 1.0) {
-        super.init(effect: nil)
-        blurEffect.setValue(color, forKeyPath: "colorTint")
-        blurEffect.setValue(alpha, forKeyPath: "colorTintAlpha")
-        blurEffect.setValue(blurRadius, forKeyPath: "blurRadius")
-        blurEffect.setValue(scale, forKeyPath: "scale")
-        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = self.blurEffect }
-        animator.fractionComplete = 1.0
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-}
---
+++ Sources/SparkUI/Views/SVisualEffectView.swift
@@ -0,0 +1,35 @@
+//
+//  SVisualEffectView.swift
+//  
+//
+//  Created by Alex Nagy on 10/05/2020.
+//
+
+import UIKit
+
+open class SVisualEffectView: UIVisualEffectView {
+    
+    private var animator: UIViewPropertyAnimator!
+    private let blurEffect = (NSClassFromString("_UICustomBlurEffect") as! UIBlurEffect.Type).init()
+    
+    public init(style: UIBlurEffect.Style, intensity: CGFloat) {
+        super.init(effect: nil)
+        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = UIBlurEffect(style: style) }
+        animator.fractionComplete = intensity
+    }
+    
+    public init(color: UIColor? = nil, alpha: CGFloat = 0.5, blurRadius: CGFloat = 3, scale: CGFloat = 1.0) {
+        super.init(effect: nil)
+        blurEffect.setValue(color, forKeyPath: "colorTint")
+        blurEffect.setValue(alpha, forKeyPath: "colorTintAlpha")
+        blurEffect.setValue(blurRadius, forKeyPath: "blurRadius")
+        blurEffect.setValue(scale, forKeyPath: "scale")
+        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = self.blurEffect }
+        animator.fractionComplete = 1.0
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
