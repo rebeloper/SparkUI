f7070c727651d9fcee837924032f9736a46d22b6
Alex Nagy <therebeloper@gmail.com>
2020-05-15 12:26:55 +0300

    public

--- Sources/SparkUI/Utils/SDebug.swift
+++ Sources/SparkUI/Utils/SDebug.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct SDebug {
-    struct Message {
-        struct FatalError {
+public struct SDebug {
+    public struct Message {
+        public struct FatalError {
             static let unknownDefault = "@unknown default has not been implemented"
             static let initCoder = "init(coder:) has not been implemented"
         }
