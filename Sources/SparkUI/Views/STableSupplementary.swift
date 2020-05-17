be5590c244f7b26c9acea1dba13092c1b72c6684
Alex Nagy <therebeloper@gmail.com>
2020-05-17 16:51:49 +0300

    STableSupplementary

---
+++ Sources/SparkUI/Views/STableSupplementary.swift
@@ -0,0 +1,65 @@
+//
+//  STableSupplementary.swift
+//  
+//
+//  Created by Alex Nagy on 17/05/2020.
+//
+
+import UIKit
+import Layoutless
+
+open class STableSupplementary<T>: UITableViewHeaderFooterView {
+    
+    open var item: T? {
+        didSet {
+            layoutViews()
+            configureViews(for: self.item)
+        }
+    }
+
+    public var indexPath: IndexPath?
+    public var reuse = true
+
+    public let container = UIView()
+    
+    override public init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func prepareForReuse() {
+        super.prepareForReuse()
+        if !reuse {
+            layoutViews()
+            configureViews(for: self.item)
+        }
+    }
+
+    open func layoutViews() {
+        stack(.vertical)(
+            container
+        ).fillingParent().layout(in: self)
+    }
+
+    open func configureViews(for item: T?) {
+        setBackgroundColor()
+    }
+    
+    open func assign() {}
+    open func addActions() {}
+    open func onNext() {}
+    
+    public func setBackgroundColor(_ backgroundColor: UIColor,
+                                   containerBackgroundColor: UIColor) {
+        self.backgroundColor = backgroundColor
+        container.backgroundColor = containerBackgroundColor
+    }
+    
+    public func setBackgroundColor(all color: UIColor = .systemBackground) {
+        setBackgroundColor(color, containerBackgroundColor: color)
+    }
+}
+
