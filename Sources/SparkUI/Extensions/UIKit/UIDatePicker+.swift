f1018352b0e24e52a2149aecdb27659185c1df12
Alex Nagy <therebeloper@gmail.com>
2020-05-18 10:27:53 +0300

    UIDatePicker+

---
+++ Sources/SparkUI/Extensions/UIKit/UIDatePicker+.swift
@@ -0,0 +1,29 @@
+//
+//  UIDatePicker+.swift
+//  
+//
+//  Created by Alex Nagy on 18/05/2020.
+//
+
+import UIKit
+
+extension UIDatePicker {
+    
+    @discardableResult
+    func date(_ date: Date) -> UIDatePicker {
+        self.date = date
+        return self
+    }
+    
+    @discardableResult
+    func datePickerMode(_ datePickerMode: UIDatePicker.Mode) -> UIDatePicker {
+        self.datePickerMode = datePickerMode
+        return self
+    }
+    
+    @discardableResult
+    func addAction(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) -> UIDatePicker {
+        self.addTarget(target, action: action, for: controlEvents)
+        return self
+    }
+}
