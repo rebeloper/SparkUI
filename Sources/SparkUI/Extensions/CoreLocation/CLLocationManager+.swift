2352b5f058c8c5c9c4adfdd388269eccbb2b2ac8
Alex Nagy <therebeloper@gmail.com>
2020-05-22 10:59:06 +0300

    locationServicesEnabled

--- Sources/SparkUI/Extensions/CoreLocation/CLLocationManager+.swift
+++ Sources/SparkUI/Extensions/CoreLocation/CLLocationManager+.swift
@@ -11,19 +11,27 @@ public extension CLLocationManager {
     
     @discardableResult
     func delegate(_ delegate: CLLocationManagerDelegate) -> CLLocationManager {
-        self.delegate = delegate
+        if CLLocationManager.locationServicesEnabled() {
+            self.delegate = delegate
+        } else {
+            Alert.showError(message: "Location Service disabled")
+        }
         return self
     }
     
     @discardableResult
     func activityType(_ activityType: CLActivityType) -> CLLocationManager {
-        self.activityType = activityType
+        if CLLocationManager.locationServicesEnabled() {
+            self.activityType = activityType
+        }
         return self
     }
     
     @discardableResult
     func desiredAccuracy(_ desiredAccuracy: CLLocationAccuracy) -> CLLocationManager {
-        self.desiredAccuracy = desiredAccuracy
+        if CLLocationManager.locationServicesEnabled() {
+            self.desiredAccuracy = desiredAccuracy
+        }
         return self
     }
     
