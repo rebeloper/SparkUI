3c702a3539bb48c81185cf9f3fdf278fe23f6bdf
Alex Nagy <therebeloper@gmail.com>
2020-05-23 12:26:30 +0300

    switchRootViewController

--- Sources/SparkUI/Extensions/UIKit/UIWindow+.swift
+++ Sources/SparkUI/Extensions/UIKit/UIWindow+.swift
@@ -5,6 +5,7 @@
 //  Created by Alex Nagy on 07/03/2020.
 //
 
+#if canImport(UIKit) && os(iOS)
 import UIKit
 
 public extension UIWindow {
@@ -43,4 +44,42 @@ public extension UIWindow {
         return window
     }
 }
+
+// MARK: - Methods
+public extension UIWindow {
+
+    /// SparkUI: Switch current root view controller with a new view controller.
+    ///
+    /// - Parameters:
+    ///   - viewController: new view controller.
+    ///   - animated: set to true to animate view controller change (default is true).
+    ///   - duration: animation duration in seconds (default is 0.5).
+    ///   - options: animation options (default is .transitionFlipFromRight).
+    ///   - completion: optional completion handler called after view controller is changed.
+    func switchRootViewController(
+        to viewController: UIViewController,
+        animated: Bool = true,
+        duration: TimeInterval = 0.5,
+        options: UIView.AnimationOptions = .transitionFlipFromRight,
+        _ completion: (() -> Void)? = nil) {
+
+        guard animated else {
+            rootViewController = viewController
+            completion?()
+            return
+        }
+
+        UIView.transition(with: self, duration: duration, options: options, animations: {
+            let oldState = UIView.areAnimationsEnabled
+            UIView.setAnimationsEnabled(false)
+            self.rootViewController = viewController
+            UIView.setAnimationsEnabled(oldState)
+        }, completion: { _ in
+            completion?()
+        })
+    }
+
+}
+
+#endif
 
