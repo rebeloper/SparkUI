4992cd3f3a3d3d29c43db2fe4518af46d44036ea
Alex Nagy <therebeloper@gmail.com>
2020-05-28 11:18:58 +0300

    prettyPrintedJSONString

---
+++ Sources/SparkUI/Extensions/Foundation/Data+.swift
@@ -0,0 +1,18 @@
+//
+//  Data+.swift
+//  
+//
+//  Created by Alex Nagy on 28/05/2020.
+//
+
+import Foundation
+
+public extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
