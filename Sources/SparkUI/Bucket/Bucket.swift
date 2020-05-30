1e262bc7695dfd08481de06f533192bd23e09964
Alex Nagy <therebeloper@gmail.com>
2020-05-30 10:02:41 +0300

    debug

--- Sources/SparkUI/Bucket/Bucket.swift
+++ Sources/SparkUI/Bucket/Bucket.swift
@@ -11,11 +11,21 @@ import Signals
 /// 🧙‍♂️ Observable object that emits signals that can be subscribed to
 public class Bucket<T> {
     
+    private var shouldDebug: Bool = false
+    
     /// 🧙‍♂️ The `Signal` of the `Bucket` that creates a number of signals that can be subscribed to
     public let value = Signal<T>()
     
     /// 🧙‍♂️ Gets the latest value of the `Bucket`
-    public var getValue: T
+    public var getValue: T {
+        didSet {
+            if shouldDebug {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "MMM d, HH:mm:ss:SSSZ"
+                print("🧙‍♂️ Bucket ~~> [\(Thread.isMainThread ? "" : "!")M] [\(dateFormatter.string(from: Foundation.Date()))] ~~> did set value: \(getValue)")
+            }
+        }
+    }
     
     /// 🧙‍♂️ Sets a new value into the Bucket
     /// - Parameter value: The new value of the `Bucket`.
@@ -31,3 +41,13 @@ public class Bucket<T> {
         setValue(initialValue)
     }
 }
+
+public extension Bucket {
+    
+    /// 🧙‍♂️ Debugs the `Bucket` in console
+    func /// 🧙‍♂️ Initializes a `Bucket` with an initial value() -> Bucket {
+        shouldDebug = true
+        return self
+    }
+}
+
