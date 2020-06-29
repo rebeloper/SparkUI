70d685c499de01bd8256527d37214e24ac9b6c5c
Alex Nagy <therebeloper@gmail.com>
2020-06-29 13:59:47 +0300

    removed background colour

--- Sources/SparkUI/Views/SCornerShadowView.swift
+++ Sources/SparkUI/Views/SCornerShadowView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public class SCornerShadowView: SView {
+public class SCornerShadowView: UIView {
     
     private var shadowRadius: CGFloat
     private var cornerRadius: CGFloat
