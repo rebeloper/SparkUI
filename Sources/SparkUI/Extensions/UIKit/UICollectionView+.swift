5857b474f43edd187d1d670ccad9fa1940f6cefa
Alex Nagy <therebeloper@gmail.com>
2020-07-01 10:58:52 +0300

    UICollectionView+

---
+++ Sources/SparkUI/Extensions/UIKit/UICollectionView+.swift
@@ -0,0 +1,71 @@
+//
+//  UICollectionView+.swift
+//  
+//
+//  Created by Alex Nagy on 01/07/2020.
+//
+
+import UIKit
+
+public extension UICollectionView {
+    
+    @discardableResult
+    func backgroundColor(_ backgroundColor: UIColor) -> UICollectionView {
+        self.backgroundColor = backgroundColor
+        return self
+    }
+    
+    @discardableResult
+    func showsVerticalScrollIndicator(_ showsVerticalScrollIndicator: Bool) -> UICollectionView {
+        self.showsVerticalScrollIndicator = showsVerticalScrollIndicator
+        return self
+    }
+    
+    @discardableResult
+    func showsHorizontalScrollIndicator(_ showsHorizontalScrollIndicator: Bool) -> UICollectionView {
+        self.showsHorizontalScrollIndicator = showsHorizontalScrollIndicator
+        return self
+    }
+    
+    @discardableResult
+    func delegate(_ delegate: UIViewController) -> UICollectionView {
+        self.delegate = delegate as? UICollectionViewDelegate
+        return self
+    }
+    
+    @discardableResult
+    func dataSource(_ dataSource: UIViewController) -> UICollectionView {
+        self.dataSource = dataSource as? UICollectionViewDataSource
+        return self
+    }
+    
+    @discardableResult
+    func registerCell(_ cellClass: AnyClass?, forCellWithReuseIdentifier identifier: String) -> UICollectionView {
+        self.register(cellClass.self, forCellWithReuseIdentifier: identifier)
+        return self
+    }
+    
+    @discardableResult
+    func registerSupplementaryView(_ viewClass: AnyClass?, forSupplementaryViewOfKind elementKind: String, withReuseIdentifier identifier: String) -> UICollectionView {
+        self.register(viewClass, forSupplementaryViewOfKind: elementKind, withReuseIdentifier: identifier)
+        return self
+    }
+    
+    @discardableResult
+    func dragInteractionEnabled(_ dragInteractionEnabled: Bool) -> UICollectionView {
+        self.dragInteractionEnabled = dragInteractionEnabled
+        return self
+    }
+    
+    @discardableResult
+    func dragDelegate(_ dragDelegate: UIViewController) -> UICollectionView {
+        self.dragDelegate = dragDelegate as? UICollectionViewDragDelegate
+        return self
+    }
+    
+    @discardableResult
+    func dropDelegate(_ dropDelegate: UIViewController) -> UICollectionView {
+        self.dropDelegate = dropDelegate as? UICollectionViewDropDelegate
+        return self
+    }
+}
