d0a9e8eb5e1967d9bc87714c104be7e720e73d2e
Alex Nagy <therebeloper@gmail.com>
2020-07-01 12:16:13 +0300

    public

--- Sources/SparkUI/Views/SCollectionView.swift
+++ Sources/SparkUI/Views/SCollectionView.swift
@@ -9,23 +9,23 @@ import UIKit
 
 public class SCollectionView: UICollectionView {
     
-    init(layout: UICollectionViewFlowLayout) {
+    public init(layout: UICollectionViewFlowLayout) {
         super.init(frame: .zero, collectionViewLayout: layout)
     }
     
-    init(with layout: UICollectionViewFlowLayout, and backgroundColor: UIColor = .systemBackground) {
+    public init(with layout: UICollectionViewFlowLayout, and backgroundColor: UIColor = .systemBackground) {
         super.init(frame: .zero, collectionViewLayout: layout)
         self.backgroundColor = backgroundColor
     }
     
-    init(with layout: UICollectionViewFlowLayout, delegateAndDataSource: UIViewController?, backgroundColor: UIColor = .systemBackground) {
+    public init(with layout: UICollectionViewFlowLayout, delegateAndDataSource: UIViewController?, backgroundColor: UIColor = .systemBackground) {
         super.init(frame: .zero, collectionViewLayout: layout)
         self.delegate = delegateAndDataSource as? UICollectionViewDelegate
         self.dataSource = delegateAndDataSource as? UICollectionViewDataSource
         self.backgroundColor = backgroundColor
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
